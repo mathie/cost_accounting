cc81515ed1d62b565eced16f06ae94e1508dc3bf
Graeme Mathieson <mathie@woss.name>
2011-09-12 14:20:23 +0100

    rails new cost_accounting -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CostAccounting::Application.config.secret_token = '8cf2d1cf15d5cbe38f2fafdd374a783087b130489b7f0ff02c9f03d3ac831bbe4114a432babec4ed57a93d7bfaed33e2be1bdbecc6a2c6c482034c7d8e5c36fc'
