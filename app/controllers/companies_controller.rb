710b5cbca031399f79203e130e150d6b65118203
Graeme Mathieson <mathie@woss.name>
2011-09-18 12:05:08 +0100

    Spruce up the company CRUD.
    * Add in edit and delete actions.
    * Add a blank slate for first visit.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,12 +12,34 @@ class CompaniesController < ApplicationController
     @company = current_user.companies.new
   end
 
+  def edit
+    @company = current_user.companies.find(params[:id])
+  end
+
   def create
     @company = current_user.companies.build(params[:company])
     if @company.save
       redirect_to companies_path, notice: 'Company successfully created'
     else
       render action: 'new'
+    end
+  end
+
+  def update
+    @company = current_user.companies.find(params[:id])
+    if @company.update_attributes(params[:company])
+      redirect_to companies_path, notice: 'Company successfully updated'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @company = current_user.companies.find(params[:id])
+    if @company.destroy
+      redirect_to companies_path, notice: 'Company successfully deleted'
+    else
+      redirect_to companies_path, alert: 'Company could not be deleted'
     end
   end
 end
